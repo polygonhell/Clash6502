0b1019158f0bb943267dfc71c64fe638bde9d9b4
Rob <polygonhell@gmail.com>
2015-08-22 16:09:08 -0700

    remove illega STA # entry replace with 65C02 BIT # instruction, no implementation

--- Alu.hs
+++ Alu.hs
@@ -29,7 +29,7 @@ data AddrOp = AONone
             deriving (Show, Eq)
 
 
-addrMode :: Unsigned 2 -> Unsigned 3 -> Unsigned 3-> (AddrMode, AddrOp)
+addrMode :: Unsigned 2 -> Unsigned 3 -> Unsigned 3 -> (AddrMode, AddrOp)
 addrMode 1 0 _ = (ZpInd, AOPreAddX)
 addrMode 1 1 _ = (Zp, AONone)
 addrMode 1 2 _ = (Imm, AONone)
@@ -48,22 +48,25 @@ data AluOp = ORA
            | LDA
            | CMP
            | SBC
+           | BIT
+           | ILLEGAL
            deriving (Show, Eq)
 
-aluOp :: Unsigned 2 -> Unsigned 3 -> AluOp
-aluOp 1 0 = ORA
-aluOp 1 1 = AND
-aluOp 1 2 = EOR
-aluOp 1 3 = ADC
-aluOp 1 4 = STA
-aluOp 1 5 = LDA
-aluOp 1 6 = CMP
-aluOp 1 7 = SBC
+aluOp :: Unsigned 2 -> Unsigned 3 -> Unsigned 3 -> AluOp
+aluOp 1 _ 0 = ORA
+aluOp 1 _ 1 = AND
+aluOp 1 _ 2 = EOR
+aluOp 1 _ 3 = ADC
+aluOp 1 2 4 = BIT  -- Store Immediate is mising -- it's BIT # on 65C02
+aluOp 1 _ 4 = STA
+aluOp 1 _ 5 = LDA
+aluOp 1 _ 6 = CMP
+aluOp 1 _ 7 = SBC
 
 
 decodeInstruction :: CpuState -> Byte -> CpuState
 decodeInstruction st x = st' where
-  o = aluOp dm opBits
+  o = aluOp dm addrBits opBits
   (m, ao) = addrMode dm addrBits opBits
   st' = st{rAluOp = o, rAddrMode = m, rAddrOp = ao}
   dm = resize x
